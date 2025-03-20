5596f3ee219a4d9f89f10ee01b1fb8d9204107be
iamkmd <124434933+iamkmd@users.noreply.github.com>
2025-03-21 02:56:07 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
 $(function(){
+
+    var mobileMenuToggle = function(){
+        $('.js-menu-toggle').on('click',function(e){
+            e.preventDefault();
+            if( $('body').hasClass('mobile-menu-active') ){
+                $('body').removeClass('mobile-menu-active');
+            } else{
+                $('body').addClass('mobile-menu-active');
+            }
+        });
+
+        $(window).resize(function(){
+            var $this = $(this);
+            if($this.width() > 768){
+                if( $('body').hasClass('mobile-menu-active') ){
+                    $('body').removeClass('mobile-menu-active');
+                } 
+            }
+        }).resize();
+
+         $('body').click(function() {
+         $(this).removeClass('mobile-menu-active');
+      });
+
+     $('.js-mobile-toggle').on('click',function(e) {
+        e.preventDefault();
+        $('body').removeClass('mobile-menu-active');
+
+     });
+    $('.mobile-menu,.js-menu-toggle').on('click',function(e) {
+        e.stopPropagation();
+
+   })
+};
+mobileMenuToggle();
+
+
     var swiperinit = function(){
         new Swiper('.testmonialSwiper',{
             loop: true,
@@ -6,7 +43,7 @@ $(function(){
             speed: 700,
             navigation:{
                 nextEl:".custom-swiper-button-next",
-                prevEl:".custom-swiper-button-next",
+                prevEl:".custom-swiper-button-prev",
             },
             pagination:{
                 el:".swiper-pagination",
